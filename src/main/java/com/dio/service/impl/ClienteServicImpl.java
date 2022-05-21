bd262e22be8dc76dbddb2c3a876115111e5f015b
Pedro Barossi <pedro3barossi@gmail.com>
2022-05-21 17:55:00 -0300

    Modelo copiado - Erro ao iniciar
    Copiado o modelo do vídeo "Praticando com Spring: Conhecendo o Projeto Base", encontrei um erro ao dar Run

---
+++ src/main/java/com/dio/service/impl/ClienteServicImpl.java
@@ -0,0 +1,31 @@
+package com.dio.service.impl;
+
+import com.dio.model.Cliente;
+import com.dio.service.ClienteService;
+
+public class ClienteServicImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
